37f57b813b13a7bbb7dfc05b32592f384bda72ff
coke727 <jorgecuadradosaez@gmail.com>
2016-05-02 01:10:24 +0200

    user confirmation popups

--- js/configuration.js
+++ js/configuration.js
@@ -8,4 +8,20 @@ $(document).ready(function($) {
       $(".accordion-content").not($(this).next()).slideUp('fast');
 
     });
+
+    $("#general").click(function(event) {
+    	var frequency = document.getElementById("frequency").value;
+
+    	if(frequency < 10) {
+        	if( !confirm('A low value for frequency can spend so much power. Are you sure?') ) 
+            	event.preventDefault();
+        }
+    });
+
+    $("#scp").click(function(event) {
+    	if( !confirm('If you are using power saving mode 3 the scp frequency could be not so acurated due to the hibernation periods.') ) {
+            	event.preventDefault();
+        }
+    });
 });
+
